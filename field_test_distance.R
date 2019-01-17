0115a56abd344fc332be4afa5fc79c94c3f062d7
Ludwig, Marvin <marvin.ludwig@geo.uni-marburg.de>
2019-01-17 14:06:53 +0100

    proper function

--- field_test_distance.R
+++ field_test_distance.R
@@ -1,29 +1,26 @@
 # distance from triangulation point to reference point
+
 library(lubridate)
+library(raster)
+
+ref_distance <- function(tri, ref){
+  # find out wich reference point matches with the triangulation by looking at the timestamp
+  tri$refpoint <- NA
+  tri$method <- NA
+  for(i in seq(nrow(ref))){
+    tri$refpoint[which(tri$timestamp > ref$start[i] & tri$timestamp < ref$end[i])] <- ref$name[i]
+    tri$method[which(tri$timestamp > ref$start[i] & tri$timestamp < ref$end[i])] <- ref$method[i]
+  }
+  
+  
+  # calculate distance from each point to the reference point
+  tri <- na.omit(tri)
+  tri$refdist <- 0
+  
+  for(j in seq(nrow(tri))){
+    tri$refdist[j] <- pointDistance(c(tri$pos.utm.X[j], tri$pos.utm.Y[j]), ref[which(tri$refpoint[j] == ref$name),7:8], lonlat = FALSE)
+  }
+  return(tri)
+}
 
 
-source("~/repositories/envimaR/R/getEnvi.R")
-p <- getEnvi("/home/hanna/rteu/data/")
-s <- getEnvi("/home/hanna/rteu/scripts/")
-
-
-tri <- readRDS(paste0(p$triangulations$here, "field_test_triangulation_a3.RDS"))
-ref <- readRDS(paste0(p$reference_position$here, "field_test_reference_points.RDS"))
-
-
-
-# test with rest1
-# filter through timestamps
-
-r1 <- tri[tri$timestamp > ref$start[1] & tri$timestamp < ref$end[1],]
-
-r1sp <- r1
-coordinates(r1sp) <- ~ pos.X + pos.Y
-projection(r1sp) <- CRS("+proj=longlat +datum=WGS84")
-
-refsp <- ref
-coordinates(refsp) <- ~ pos.X + pos.Y
-projection(refsp) <- CRS("+proj=longlat +datum=WGS84")
-
-
-mapview(r1sp, color = "red") + mapview(refsp[1,])
