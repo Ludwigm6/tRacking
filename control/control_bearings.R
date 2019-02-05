d54998d5ba5149ea02f720490ba75a48bccc8798
Ludwig, Marvin <marvin.ludwig@geo.uni-marburg.de>
2019-02-05 18:18:07 +0100

    test different parameters

--- control/control_bearings.R
+++ control/control_bearings.R
@@ -1,43 +1,76 @@
 
-library(plyr)
-
-source("~/repositories/envimaR/R/getEnvi.R")
-p <- getEnvi("/home/marvin/rteu/field_test/data/")
-s <- getEnvi("/home/marvin/rteu/field_test/scripts/")
+source("/home/marvin/rteu/field_test/scripts/tRacking/src/init_bearings.R")
 
 
-# source bearing function
-source(paste0(s$tRacking$src$here, "calc_bearings.R"))
-source(paste0(s$modified$here, "srvFileIO_reverse.R"))
-source(paste0(s$modified$here, "srvFilters_reverse.R"))
+# calibration or no calibration
+#--------------------------------------
+
+calib <- read.csv(paste0(p$logger_data$here, "calibrations.csv"))
+
+logger_calib <- logger_data
+logger_calib <- join(logger_calib, calib, by = "receiver")
+logger_calib$max_signal <- logger_calib$max_signal + logger_calib$correction
 
 
-# antenna locations
-receiver_data <- read.csv(paste0(p$gps_data$here, "antennas.csv"))
+bearings_cal <- lapply(list(logger_data, logger_calib), function(l){
+  # calculate all bearings
+  tm_signals <- time_match_signals(l, station_time_error = 0.3)
+  
+  
+  # bearings with 14 dbLoss
+  # dBLoss -> signal difference to same antenna rotated 90 degree
+  registerDoParallel(cores=detectCores())
+  bearings <- doa(tm_signals, receiver_data, dBLoss = 14)
+  bearings$timestamp <- as.POSIXct(bearings$timestamp, origin = "1970-01-01", tz = "UTC")
+  stopImplicitCluster()
+  bearings <- match_bearings(bearings, ref)
+  bearings <- na.omit(bearings)
+  return(bearings)
+})
 
-# read all logger data
-logger_data <- read_logger_folder(p$logger_data$log_fieldtest$here)
+bearings_cal_test <- do.call(rbind, bearings_cal)
+bearings_cal_test$calibration <- c(rep("no", 6852), rep("yes", 6760))
 
-# calculate all bearings
-b <- calc_bearings(logger_data = logger_data,
-                   receiver_data = receiver_data,
-                   function_path = s$modified$here,
-                   FOI = 150203)
+saveRDS(bearings_cal_test, paste0(p$bearings$here, "bearings_calibration.RDS"))
 
 
-# add reference point information
-ref <- read.csv(paste0(p$gps_data$here, "lut_measures.csv"), stringsAsFactors = FALSE)
-ref$start <- as.POSIXct(ref$start, origin = "1970-01-01",tz = "UTC")
-ref$end <- as.POSIXct(ref$end, origin = "1970-01-01",tz = "UTC")
+# continue with calibrated bearings
+#------------------------------------------
+# test different dBLosses
+bearings_loss <- lapply(seq(1,25,1), function(j){
+  tm_signals <- time_match_signals(logger_calib, station_time_error = 0.3)
+  registerDoParallel(cores=detectCores())
+  bearings <- doa(tm_signals, receiver_data, dBLoss = j)
+  bearings$timestamp <- as.POSIXct(bearings$timestamp, origin = "1970-01-01", tz = "UTC")
+  stopImplicitCluster()
+  
+  bearings <- match_bearings(bearings, ref)
+  bearings <- na.omit(bearings)
+  bearings$dbloss <- j
+  return(bearings)
+  
+})
+
+bearings_loss_test <- do.call(rbind, bearings_loss)
+
+boxplot(bearings_cal_test$angle_error[bearings_cal_test$method != "WALK"] ~ bearings_cal_test$calibration[bearings_cal_test$method != "WALK"])
+
+saveRDS(bearings_loss_test, paste0(p$bearings$here, "bearings_dbloss.RDS"))
+
+# final bearings with calibration and 15 dbLoss
+#---------------------------------------
+tm_signals <- time_match_signals(logger_calib, station_time_error = 0.3)
+registerDoParallel(cores=detectCores())
+bearings <- doa(tm_signals, receiver_data, dBLoss = 15)
+bearings$timestamp <- as.POSIXct(bearings$timestamp, origin = "1970-01-01", tz = "UTC")
+stopImplicitCluster()
+
+bearings <- match_bearings(bearings, ref)
+bearings <- na.omit(bearings)
+
+saveRDS(bearings, paste0(p$bearings$here, "bearings.RDS"))
 
 
-b$id <- NA
-for(i in seq(nrow(ref))){
-  b$id[which(b$timestamp > ref$start[i] & b$timestamp < ref$end[i])]  <- ref$id[i]
-}
 
-b <- join(b, ref, by = "id")
-
-write.csv(b, paste0(p$bearings$here, "bearings.csv"), row.names = FALSE)
 
 
--- control/control_triangulations.R
+++ control/control_triangulations.R
@@ -3,32 +3,114 @@
 source("~/rteu/field_test/scripts/tRacking/src/init_triangulation.R")
 
 # filter bearings
-bearings <- bearings[bearings$antennas > 3,]
-bearings <- bearings[bearings$method == "MOVE",]
-
-tri <- triangulate(receiver_data, bearings, 
-                   time_error_inter_station = 2,
-                   only_one = F,
-                   angles_allowed = c(30,150),
-                   tm_method = "tm",
-                   tri_option = "two_strongest",
-                   spar = 3, 
-                   progress = F)
-
-tri <- na.omit(tri)
+bearings <- bearings[as.numeric(bearings$antennas) > 1,]
+bearings <- bearings[bearings$method %in% c("RUHE"),]
 
 
-tri <- tri_match(tri, ref)
+# try different minimal angles
+#------------------------------------------------------
 
-trisp <- tri_sp(tri)
+tri_min_angle <- lapply(seq(0,160,10), function(i){
+  
+  tri <- triangulate(receiver_data, bearings, 
+                     time_error_inter_station = 0.5,
+                     only_one = F,
+                     angles_allowed = c(i,170),
+                     tm_method = "tm",
+                     tri_option = "two_strongest",
+                     progress = F)
+  tri <- na.omit(tri)
+  tri <- tri_match(tri, ref)
+  tri <- tri_error(tri)
+  tri$min_angle <- i
+  return(tri)
+  
+})
 
-tri <- tri_error(tri)
+tri_min_angle <- do.call(rbind, tri_min_angle)
 
-boxplot(tri$refdist ~ tri$r_location)
-
-refsp <- ref
-coordinates(refsp) <- ~ pos.X + pos.Y
-projection(refsp) <- CRS("+proj=longlat +datum=WGS84")
+saveRDS(tri_min_angle, paste0(p$triangulations$here, "tri_minimal_angles.RDS"))
 
 
-mapview(trisp, zcol = "r_location") + mapview(refsp, color = "red")
+# try different maximum angles, minimum set to 40
+#----------------------------------------------------
+
+tri_max_angle <- lapply(seq(50,180,10), function(i){
+  
+  tri <- triangulate(receiver_data, bearings, 
+                     time_error_inter_station = 0.5,
+                     only_one = F,
+                     angles_allowed = c(40,i),
+                     tm_method = "tm",
+                     tri_option = "two_strongest",
+                     progress = F)
+  tri <- na.omit(tri)
+  tri <- tri_match(tri, ref)
+  tri <- tri_error(tri)
+  tri$max_angle <- i
+  return(tri)
+  
+})
+
+tri_max_angle <- do.call(rbind, tri_max_angle)
+saveRDS(tri_max_angle, paste0(p$triangulations$here, "tri_maximal_angles.RDS"))
+
+
+# try different time errors
+#-------------------------------------------------------------
+tri_error_time <- lapply(seq(0.1,2,0.1), function(t){
+  
+  tri <- triangulate(receiver_data, bearings, 
+                     time_error_inter_station = t,
+                     only_one = F,
+                     angles_allowed = c(40,170),
+                     tm_method = "tm",
+                     tri_option = "two_strongest",
+                     progress = F)
+  tri <- na.omit(tri)
+  tri <- tri_match(tri, ref)
+  tri <- tri_error(tri)
+  tri$error_time <- t
+  return(tri)
+
+})
+
+tri_error_time <- do.call(rbind, tri_error_time)
+saveRDS(tri_error_time, paste0(p$triangulations$here, "tri_error_time.RDS"))
+
+
+
+# triangulation with different station combinations
+#------------------------------------------------------------
+
+
+station <-c("S1", "S2", "S3")
+
+tri_stations <- lapply(seq(3), function(i){
+  b <- bearings[bearings$Station != station[i],]
+  
+  tri <- triangulate(receiver_data, b, 
+                     time_error_inter_station = 0.7,
+                     only_one = F,
+                     angles_allowed = c(0,180),
+                     tm_method = "tm",
+                     tri_option = "two_strongest",
+                     spar = 3, 
+                     progress = F)
+  
+  tri <- na.omit(tri)
+  tri <- tri_match(tri, ref)
+  tri <- tri_error(tri)
+  tri$unused_station <- i
+  return(tri)
+  
+})
+tri_s <- do.call(rbind, tri_stations)
+
+boxplot(tri_stations[[]]$refdist ~ tri_stations[[3]]$r_location, ylim = c(0,100))
+
+s1_sp <- tri_sp(tri_stations[[2]])
+mapview(s1_sp, zcol = "r_location") + mapview(refsp, color = "red") + mapview(antennas, color = "green")
+
+
+
